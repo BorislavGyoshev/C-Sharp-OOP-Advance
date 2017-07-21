8d5c76494e67afe70bbdca7dc98a75eaaf87453e
BorislavGyoshev <b.gioshev@abv.bg>
2017-07-21 08:25:40 +0300

    Add files via upload

---
+++ InterfacesAndAbstractionLab/01_Shapes/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _01_Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width, int height)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+            }
+        }
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                width = value;
+            }
+        }
+
+        public void Draw()
+        {
+            DrawLine(this.Width, '*', '*');
+            for (int i = 1; i < this.Height - 1; ++i)
+                DrawLine(this.Width, '*', ' ');
+            DrawLine(this.Width, '*', '*');
+        }
+        private void DrawLine(int width, char end, char mid)
+        {
+            Console.Write(end);
+            for (int i = 1; i < width - 1; ++i)
+                Console.Write(mid);
+            Console.WriteLine(end);
+        }
+    }
+}
