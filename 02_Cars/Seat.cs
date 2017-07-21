8d686b0a390acc97f5c1f35423648ee6a27815b9
BorislavGyoshev <b.gioshev@abv.bg>
2017-07-21 09:34:04 +0300

    Add files via upload

---
+++ 02_Cars/Seat.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Seat : ICar
+    {
+        private string color;
+        private string model;
+
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Color
+        {
+            get { return color; }
+            private set { color = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            private set { model = value; }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Seat {Model}");
+            sb.AppendLine(Start());
+            sb.Append(Stop());
+            return sb.ToString();
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
---
+++ 02_Cars/Tesla.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        private string color;
+        private string model;
+        private int battery; 
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Color
+        {
+            get { return color; }
+            private set { color = value; }
+        }
+        public string Model
+        {
+            get{ return model; }
+            private set { model = value; }
+        }
+        public int Battery
+        {
+            get { return battery; }
+            private set { battery = value; }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Bateries");
+            sb.AppendLine(Start());
+            sb.Append(Stop());
+            return sb.ToString();
+        }
+        public string Start()
+        {
+            return "Engine start";
+        }
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+    }
+}
